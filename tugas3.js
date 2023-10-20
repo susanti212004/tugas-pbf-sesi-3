1de4610dc7598bad31094331dd640bfe37d84cc4
susanti212004 <123722597+susanti212004@users.noreply.github.com>
2023-10-20 20:01:06 +0700

    Add files via upload

---
+++ tugas3.js
@@ -0,0 +1,24 @@
+// Variabel dengan tipe object
+var restaurantObject = {
+    name : "Aku",
+    city : "Sukabumi Jampang",
+    favoriteDrink : "Matcha Ice",
+    favoriteFood : "Fried Chicken",
+    isVegan: false
+};
+
+ /*
+ Variabel dengan tipe array
+ menggunakan kurung kotak sebagai penanda array
+ dan kurung kurawal sebagai pengisi value array
+ */
+var restaurantArray = [
+    {
+    name : "Aku",
+    city : "Sukabumi Jampang",
+    favoriteDrink : "Matcha Ice",
+    favoriteFood : "Fried Chicken",
+    isVegan: false
+    }
+];
+console.log(restaurantObject)
